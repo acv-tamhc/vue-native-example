672698fb3540fe730da57a2dc89add93d25ff3a5
acv-tamhc <tamhc@atmarkcafe.org>
2018-12-19 10:30:47 +0700

    init app

--- App.vue
+++ App.vue
@@ -1,35 +1,10 @@
 <template>
-  <view class="container">
-    <camera class="container" :type="this.type"/>
-  </view>
+  <setup></setup>
 </template>
 
-<style>
-.container {
-  flex: 1;
-}
-.text-color-primary {
-  color: blue;
-}
-</style>
-
 <script>
-import { Camera, Permissions } from "expo";
+import Setup from "./src/setup.vue";
 export default {
- data: function() {
-   return {
-     hasCameraPermission: false,
-     type: Camera.Constants.Type.back,
-   };
- },
- mounted: function() {
-   Permissions.askAsync(Permissions.CAMERA)
-     .then(status => {
-       hasCameraPermission = status.status == "granted" ? true : false;
-     }).catch((err)=>{
-        console.log(err);
-     });
- },
- components: { Camera },
+  components: { Setup }
 };
 </script>
