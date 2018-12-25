ab4ca03a9eac4f9a89b2a00caeaba2e7b6139319
acv-tamhc <tamhc@atmarkcafe.org>
2018-12-25 10:43:30 +0700

    list item

--- src/index.vue
+++ src/index.vue
@@ -19,7 +19,7 @@
         <nb-row :style="{ backgroundColor: '#fff' }" :size="25">
           <nb-content>
             <nb-form>
-              <nb-textarea full :rowSpan="4" bordered placeholder="Enter messages" />
+              <nb-textarea v-model='message' full :rowSpan="4" bordered placeholder="Enter messages" />
               <nb-button full :onPress="sendMessages">
                 <nb-text>Send</nb-text>
               </nb-button>
@@ -33,7 +33,7 @@
 
 <script>
 import React from "react"
-import { ListView } from "react-native"
+import { ListView, AsyncStorage, RefreshControl, FlatList } from "react-native"
 import { Col, Row, Grid } from 'react-native-easy-grid'
 import {
   Button, Icon, Text, ListItem, Root,
@@ -46,31 +46,47 @@ export default {
     return {
       ds: new ListView.DataSource({ rowHasChanged: (r1, r2) => r1 !== r2 }),
       basic: true,
-      listViewData: [
-        "Simon Mignolet",
-        "Nathaniel Clyne",
-        "Dejan Lovren",
-        "Mama Sakho",
-        "Alberto Moreno",
-        "Emre Can",
-        "Joe Allen",
-        "Phil Coutinho"
-      ]
+      listViewData: [],
+      message: '',
+      messageIndex: 0,
+      state: {
+        refreshing: false
+      }
     }
   },
   methods: {
+    _onRefresh () {
+      this.setState({refreshing: true})
+      // fetchData().then(() => {
+      //   this.setState({refreshing: false});
+      // })
+    },
+    renderRefreshControl: function() {
+      render (
+        <RefreshControl
+            refreshing={this.state.refreshing}
+            onRefresh={this._onRefresh}
+          />
+      )
+    },
     deleteRow: function(secId, rowId, rowMap) {
       rowMap[`${secId}${rowId}`].props.closeRow();
       const newData = [...this.listViewData];
       newData.splice(rowId, 1);
       this.listViewData = newData;
+      AsyncStorage.setItem('messages', JSON.stringify(this.listViewData))
     },
-    getLeftHiddenRowComponet: function(data) {
+    getLeftHiddenRowComponet: function(data, secId, rowId, rowMap) {
       return (
-        <Button full onPress={() => alert(data)}>
-          <Icon active name="information-circle" />
+        <Button full onPress={() => this.loadMessageItem(data, secId, rowId, rowMap) }>
+          <Icon active name="build" />
         </Button>
       );
+    },
+    loadMessageItem: function(data, secId, rowId, rowMap) {
+      this.message = data
+      this.messageIndex = this.listViewData.indexOf(data)
+      rowMap[`${secId}${rowId}`].props.closeRow();
     },
     getRighttHiddenRowComponet: function(data, secId, rowId, rowMap) {
       return (
@@ -80,17 +96,26 @@ export default {
       );
     },
     getListArr: function() {
+      AsyncStorage.getItem('messages').then((result) => {
+        if (result == '' || result == undefined) return
+        this.listViewData = JSON.parse(result)
+      })
+      // this.listViewData = JSON.parse(AsyncStorage.getItem('messages'))
       return this.ds.cloneWithRows(this.listViewData);
     },
     getListItemRow: function(data) {
       return (
-        <ListItem>
+        <ListItem refreshControl={() => this.renderRefreshControl() }>
           <Text>{data}</Text>
         </ListItem>
       );
     },
     sendMessages: function() {
-      console.log('send message')
+      if (this.message.length) {
+        this.listViewData.push(this.message)
+        AsyncStorage.setItem('messages', JSON.stringify(this.listViewData))
+        this.message = ''
+      }
     }
   }
 };
